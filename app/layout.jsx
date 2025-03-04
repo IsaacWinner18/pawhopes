281d0ceed51167f900aaca2bd4320d4464e7b0ec
IsaacWinner18 <winsamlab222@gmail.com>
2025-03-04 12:02:48 +0100

    testing google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,42 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Footer from "./../components/footer";
-import Header from './../components/header';
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Paw Hope Rescue",
   description: "Give them hope, one paw at a time",
-}
+};
 
-export default function RootLayout({
-  children
-}) {
+export default function RootLayout({ children }) {
   return (
-    (<html lang="en">
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={`${inter.className} overflow-x-hidden`}>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-VXH4HYMXJL"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VXH4HYMXJL');
+          `}
+        </Script>
+
         <Header />
-        <main className="min-h-screen ">
-          {children}
-        </main>
+        <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
-    </html>)
+    </html>
   );
 }
-
